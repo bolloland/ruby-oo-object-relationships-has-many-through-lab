296dd5fab9b43246ad685f52d17e851609b72209
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-06 23:49:45 -0600

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,19 @@
-
+require 'pry'
+class Appointment
+    attr_accessor :doctor, :patient, :date
+    attr_reader 
+    attr_writer
+    
+    @@all = []
+    
+    def initialize(date, patient, doctor)
+        @doctor = doctor
+        @patient = patient
+        @date = date
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+require 'pry'
+class Patient
+    attr_accessor :name
+    attr_reader 
+    attr_writer
+    
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
 
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|my_app| my_app.patient == self}
+    end
+
+    def doctors
+        appointments.map {|whom| whom.doctor}
+    end
+end
